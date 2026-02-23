99340985b82fba615669ca91bda9056f13c0d0b1
Officialakshat <165244500+Officialakshat@users.noreply.github.com>
2026-02-23 22:38:47 +0530

    update

--- script.js
+++ script.js
@@ -1,4 +1,34 @@
-let apiKay = "16f6dbcdfe4723e076394b1753066975";
-const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+let apiKey = "16f6dbcdfe4723e076394b1753066975";
+let cityInput = document.getElementById("city-name")
+let searchBtn = document.getElementById("search-btn");
+// const apiUrl = `https://api.openweathermap.org/data/2.5/weather?units=metric&q=`;
+let temp = document.getElementById("temp-data");
+let weatherIcon = document.getElementById("weather-icon")
+let now = new Date();
+console.log(now);
 
-console.log(${apiUrl})
+
+
+const getData = async (city) => {
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?units=metric&q=${city}&appid=${apiKey}`;
+    let response = await fetch(apiUrl);
+    let data = await response.json()
+    let temperature = (data.main.temp).toFixed()
+    temp.innerHTML = `${temperature}°C`;
+    if (data.weather[0].main == "clear") {
+        weatherIcon.innerHTML = `<i class="fa-solid fa-sun" style="color: rgb(255, 212, 59);"></i>`;
+    
+    } 
+}
+
+
+
+
+searchBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    getData(cityInput.value)
+})
+
+
+
+
