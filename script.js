7c626e6a24f4d31b22e6d9839c4309b85a028205
Officialakshat <165244500+Officialakshat@users.noreply.github.com>
2026-02-28 14:35:41 +0530

    update

--- script.js
+++ script.js
@@ -1,40 +1,41 @@
-let apiKey = "16f6dbcdfe4723e076394b1753066975";
-let city = document.getElementById("search-city");
+let apiKey = "&appid=16f6dbcdfe4723e076394b1753066975";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?units=metric&q=`;
+// city
+let city = document.getElementById("city").value;
+// search
 let searchBtn = document.getElementById("search-btn");
-const apiUrl = `https://api.openweathermap.org/data/2.5/weather?units=metric&q=hoshiarpur&appid=16f6dbcdfe4723e076394b1753066975`;
+// temperature
 let temperature = document.querySelectorAll(".temp-data");
+// weatherIcon
 let weatherIcon = document.getElementById("weather-icon");
-let cityName = document.getElementById("city-name");
+// wind
 let wind = document.getElementById("wind");
+// humidity
 let humidity = document.getElementById("humidity");
+// date
 let date = document.getElementById("date")
 
-async function weather() {
-  let res = await fetch(apiUrl);
+async function weather(city) {
+  let res = await fetch(`${apiUrl}${city}${apiKey}`);
   let result = await res.json();
   console.log(result);
 
-  // date
-const today = new Date();
-
-const formattedDate = today.toLocaleDateString("en-GB", {
-  day: "2-digit",
-  month: "2-digit",
-  year: "2-digit",
-});
-
-console.log(formattedDate);
-
-
+  
+  
+  function onSearch() {
+    console.log(city.value);
+  }
   // temperature
   let temp = result.main.temp;
   temperature.forEach((element) => {
     element.innerHTML = `${temp}°C`;
   });
 
+  // console.log(city);
+
   // wind
   let windData = result.wind.speed;
-  console.log(windData);
+  // console.log(windData);
   wind.innerHTML = `${windData} km/h`;
 
 
